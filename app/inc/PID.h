e857930f3fba4ef65409fa4583b30c73eb221603
liam <yzwliam@126.com>
2024-10-29 15:39:12 +0800

    🎉: start pid, add angle support

--- app/inc/PID.h
+++ app/inc/PID.h
@@ -1,12 +1,13 @@
 #ifndef __PID_H
 #define __PID_H
+#include <math.h>
 
-void cal_angel(float* yaw,float* roll,float* pitch);
+void cal_angel(double* x,double* y,double* z);
+
 // 偏航角（z轴旋转）
-double former_x;
+extern double former_x;
 // 横滚角（y轴旋转）
-double former_y;
+extern double former_y;
 // 俯仰角（x轴旋转）
-double former_z;
-
+extern double former_z;
 #endif
